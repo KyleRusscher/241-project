2c6d9f71aa6382d0a9ad670b0456fb119e1b0794
Kyle Russcher <russchky@mail.gvsu.edu>
2019-11-20 16:37:24 -0500

    added file name error checking

--- bash.sh
+++ bash.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-fileName=$((1 + RANDOM % 100000)).txt
-echo $fileName
-for var in "$@"
-do   
-    sha1sum $var | cut -d" " -f 1 >> ./${fileName}    
+fileName=$((1 + RANDOM % 100000))
+
+while [ -e ${fileName}.txt ] 
+do  
+fileName=$((1 + RANDOM % 100000))
 done
-sha1sum $fileName | cut -d" " -f 1 
-rm $fileName
+for var in "$@"
+do 
+    if [ -f $var ]
+    then
+        sha1sum $var | cut -d" " -f 1 >> ./${fileName}.txt    
+    fi
+done
+sha1sum ${fileName}.txt | cut -d" " -f 1 
+rm ${fileName}.txt
 
