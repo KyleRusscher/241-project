2b7533acf1c505ef984ad1322e1bc0683dcade7c
Kyle Russcher <russchky@mail.gvsu.edu>
2019-11-29 21:28:10 -0500

    remove print statements

--- Merkle.sh
+++ Merkle.sh
@@ -9,21 +9,15 @@ function hash_lines(){
     do
         if [ $((count%2)) -ne 0 ]
         then 
-        echo "------"
-        echo $single$i
-        echo "------"
         temp_hashes+=($(echo $i$single | sha1sum  | cut -d" " -f 1))
-        echo "$i$single" | sha1sum  | cut -d" " -f 1
         # unset single
         else
         single=$i
-        echo $single
         fi
         ((count=count+1)) 
     done
     if [ $((count%2)) -ne 0 ]
     then
-    echo "xd" ${hashes[${count} - 1]} "xd"
     temp_hashes+=($(echo ${hashes[${count} - 1]} | sha1sum | cut -d" " -f 1))
     fi
     hashes=("${temp_hashes[@]}")
@@ -39,15 +33,10 @@ do
         hashes+=($(sha1sum $var | cut -d" " -f 1))
     fi
 done
-echo ${hashes[@]}
-echo "-----------START-------------"
 while [ "${#hashes[@]}" -ne 1 ]
 do
-echo "------------AGAIN------------"
-echo "${#hashes[@]}"
 hash_lines
 done
-echo "${#hashes[@]}"
 echo ${hashes[@]}
 
 
